1ffe99cda0c7b714d0a1935389b0e511575d6ae7
tatiana0326 <tatiana05taboada@gmail.com>
2022-07-22 00:25:34 -0500

    detalle pedido

--- src/pages/page-pedidos.jsx
+++ src/pages/page-pedidos.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { Historial } from "../components/compras-component/historial/historial";
 import { Header } from "../components/producto-component/header";
+import {DetallePedido} from "../components/compras-component/detalle/detalle";
+import { ActualizarPedido } from "../components/compras-component/actualizar/actualizar-pedido";
 
 export const PagePedidos = () => {
     return (
@@ -9,6 +11,7 @@ export const PagePedidos = () => {
             <div className="p-8">
                 <Routes>
                     <Route path ="/cliente/:id" element = {<Historial/>}></Route>
+                    <Route path ="/detalle" element ={<DetallePedido/>}></Route>
                 </Routes>
             </div>
         </div>
