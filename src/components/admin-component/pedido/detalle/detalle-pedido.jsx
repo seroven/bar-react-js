9408b8bb28a4b2923a0f4ead0f5ace148471f0fc
Quispe-E <quispeaguila07@gmail.com>
2022-07-26 23:53:24 -0500

    Detalle pedido admin

--- src/components/admin-component/pedido/detalle/detalle-pedido.jsx
+++ src/components/admin-component/pedido/detalle/detalle-pedido.jsx
@@ -1,25 +1,65 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./detalle.css";
 import { ModalConfActualizar } from "./moda-conf-actualizar";
+import axios from "axios";
 
 export const DetallePedidoAdmin = () => {
   const { id } = useParams();
+  const [detalle, setDetalle] = useState([]);
+  const [productos, setProductos] = useState([]);
   const [updateProducts, setUpdateProducts] = useState(false);
   const [modalConfActualizar, setModalConfActualizar] = useState(false);
+  
 
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
 
+  const obtenerDatos = async () => {
+    let detalle = await axios.get(`http://localhost:8069/pedido/buscar/${id}`);
+    let productos = await axios.get(`http://localhost:8069/detalle/order/${id}`);
+    detalle = detalle.data[0];
+    productos = productos.data;
+    setDetalle(detalle);
+    setProductos(productos);
+    joinProductos(detalle, productos);
+    //console.log(detalle);
+  };
 
+  const joinProductos = (detalle, productos) => {
+    const newDetalle = {
+      ...detalle,
+      productos: detalle.productos.map((item) => {
+        const prod = productos.find((prod) => prod.idproducto === item.codigo);
+        return {
+          ...item,
+          cantidad: prod.cantidad,
+          subtotal: prod.subtotal,
+        };
+      }),
+    }
+    setDetalle(newDetalle);
+  };
+
+  const formaterDate = (date) => {
+    const dateFormated = new Date(date);
+    const day = dateFormated.getDate() + 1;
+    const month = dateFormated.getMonth() + 1;
+    const year = dateFormated.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
 
   return (<>
     <div className={modalConfActualizar ? "blur-md" : ""}>
       <div className="text-4xl text-center font-bold text-[#022601] flex flex-col justify-between md:flex-row">
-        <span>Pedido N°{id} | 16/07/22 </span>
-        <span>Total: S/. 1395.00</span>
+        <span>Pedido N°{id} | {" "} {formaterDate(detalle.fecha_solicitud)} </span>
+        <span>Total: {"S/." + detalle?.precio_total}</span>
       </div>
       <br />
       <div className="text-[2rem] text-[#7CA504] font-semibold">
-        <span>Valeria Rojas Cuadros - 7512543</span>
+        <span>{detalle?.cliente?.nombre} - {detalle?.cliente?.dni}</span>
       </div>
       <div className="container-data-client">
         <div>
@@ -29,7 +69,7 @@ export const DetallePedidoAdmin = () => {
           {
             updateProducts
             ? <input id="telefono" className="input-update-order"/>
-            : <span>985147125</span>
+            : <span>{detalle?.cliente?.telefono}</span>
           }
         </div>
         <div>
@@ -39,7 +79,7 @@ export const DetallePedidoAdmin = () => {
           {
             updateProducts 
             ? <input id="fecha" type = "date" className="input-update-order"/>
-            : <span>25/06/2023</span>
+            : <span>{formaterDate(detalle?.fecha_envio)}</span>
           }
         </div>
         <div className="col-start-3 col-end-4 row-start-1 row-end-3 justify-self-center">
@@ -62,7 +102,7 @@ export const DetallePedidoAdmin = () => {
           {
             updateProducts 
             ? <input id="dni_recoger" className="input-update-order"/>
-            : <span>75121453</span>
+            : <span>{detalle?.dni_recibidor}</span>
           }
         </div>
         <div>
@@ -108,32 +148,38 @@ export const DetallePedidoAdmin = () => {
       <div className="text-[2rem] text-[#022601] font-bold">
         <span>Productos</span>
       </div>
-      <div className="containter-data-products">
-        <div className="item-order-product">
-          <div className="h-full overflow-hidden rounded-xl row-start-1 row-end-3 mx-16">
-              <img className="object-cover" src="https://images.unsplash.com/photo-1598439210625-5067c578f3f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"/>
-          </div>
-          <div className="font-bold text-[1.7rem] col-start-2 col-end-4">
-            Un pinguino bien fachero 😎
-          </div>
-          <div className="grid grid-rows-2 gap-y-4">
-            <div className="flex flex-row justify-between">
-                <span className="font-semibold">Cantidad:</span>
-                <span>2</span>
-            </div>
-            <div className="flex flex-row justify-between">
-                <span className="font-semibold">Costo Unitario:</span>
-                <span>S/. 1000.00</span>
-            </div>
-          </div>
-          <div className="justify-self-center text-center">
-            <span className="font-semibold">SubTotal</span>
-            <br/>
-            <span>S/. 2000.00</span>
-          </div>
+      <div> {detalle.productos?.map((producto) => (
+          <div className="containter-data-products">
+            <div key={producto?.codigo} className="item-order-product">
+              <div className="h-full overflow-hidden rounded-xl row-start-1 row-end-3 mx-16">
+                  <img className="object-cover" src={producto?.imagen}/>
+              </div>
+              <div className="font-bold text-[1.7rem] col-start-2 col-end-4">
+              {producto?.descripcion}
+              </div>
+              <div className="grid grid-rows-2 gap-y-4">
+                <div className="flex flex-row justify-between">
+                    <span className="font-semibold">Cantidad:</span>
+                    <span>{producto?.cantidad}</span>
+                </div>
+                <div className="flex flex-row justify-between">
+                    <span className="font-semibold">Costo Unitario:</span>
+                    <span>S/. {(producto?.precio).toFixed(2)}</span>
+                </div>
+              </div>
+              <div className="justify-self-center text-center">
+                <span className="font-semibold">SubTotal</span>
+                <br/>
+                <span>{"S/. " + ( producto?.subtotal ).toFixed(2)}</span>
+              </div>
 
-        </div>
+            </div>
+          </div>
+          ))}
       </div>
+
+
+
     </div>
     <ModalConfActualizar modal = {modalConfActualizar} setModal = {setModalConfActualizar} setUpdateProducts ={setUpdateProducts}/>
   </>
